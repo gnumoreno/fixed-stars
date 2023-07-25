619356bbe441d8147f046b269e8c0583404a2fcd
Matheus Lasserre <matheuslasserre3@gmail.com>
2023-07-25 15:08:15 -0300

    Removing unused code and font style

--- src/utils/input.ts
+++ src/utils/input.ts
@@ -7,18 +7,3 @@ export const isNumeric = (num: number | string) => {
     return !isNaN(number)
 }
 
-const daysInMonth = (m: number, y: number) => {
-    switch (m) {
-        case 1:
-            return (y % 4 == 0 && y % 100) || y % 400 == 0 ? 29 : 28;
-        case 8: case 3: case 5: case 10:
-            return 30;
-        default:
-            return 31
-    }
-};
-
-const isValidDate = (d: number, m: number, y: number) => {
-    m = m - 1;
-    return m >= 0 && m < 12 && d > 0 && d <= daysInMonth(m, y);
-};
