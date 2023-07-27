7d48ffeea86cc2372cc1c93c0735cfcc162a9fc2
Moreno Garcia e Silva <morenogarciaesilva@Morenos-MacBook-Pro.local>
2023-07-27 17:15:25 -0300

    basic aspect + fix orientation

---
+++ src/utils/external/aspects/properties.ts
@@ -0,0 +1,11 @@
+import { type aspect } from "./types";
+
+const aspectProperties = [
+  { type: "conjunction", angle: 0, unicode: "\u260C" },
+  { type: "sextile", angle: 60, unicode: "\u26B9" },
+  { type: "square", angle: 90, unicode: "\u25A1" },
+  { type: "trine", angle: 120, unicode: "\u25B3" },
+  { type: "oposition", angle: 180, unicode: "\u260D" },
+];
+
+
---
+++ src/utils/external/aspects/types.d.ts
@@ -0,0 +1,11 @@
+export type aspect = {
+    astrotypeA: string,
+    astronameA: string,
+    positionA: number, // we will call position the position in the ecliptic
+    angleA: number, // we will call angle the angle in the map drawing
+    typeOfAspect: string,
+    astrotypeB: string,
+    astronameB: string,
+    positionB: number, // we will call position the position in the ecliptic
+    angleB: number, // we will call angle the angle in the map drawing
+}
