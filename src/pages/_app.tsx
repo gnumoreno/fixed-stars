198fd0f188bf52070eb7028181840dd8e4678201
gnumoreno <gnumoreno@gmail.com>
2024-01-20 17:35:49 -0300

    Try google credential response

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -10,6 +10,13 @@ const MyApp: AppType = ({ Component, pageProps }) => {
     {/* <meta name="google-signin-client_id" content="383086323854-erovqgtgonfasiljehjcect0gun8m8rl.apps.googleusercontent.com"/> */}
   </Head>
   <Script src="https://accounts.google.com/gsi/client" async defer/>
+  <Script id="google-credentials-response">
+    {`
+      function handleCredentialResponse(response) {
+        console.log(response);
+      }
+    `}
+  </Script>
   <Component {...pageProps} />;
   </>
 };
